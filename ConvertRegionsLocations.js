9cee3934bc0f33a81f06c61d903df28db7964514
Kris Hole <kris.hole@r8games.com>
2020-02-13 11:26:13 +0000

    updated location lookup

--- ConvertRegionsLocations.js
+++ ConvertRegionsLocations.js
@@ -8,45 +8,46 @@ handlers.convertRegionsToLocations = function (args, context){
 	for (const iterator of Regions) {
         switch (iterator) {
             case "NorthEurope":
-				return { messageValue: ["stockholm"] };
-
             case "WestEurope":
-				return { messageValue: ["frankfurt", "amsterdam", "london"] };
-
-            case "AustraliaEast":
-            case "AustraliaSoutheast":
-            case "SoutheastAsia":
-				return { messageValue: ["singapore"] };
-
-            case "BrazilSouth":
-				return { messageValue: ["sao_paulo"] };
-
-            case "NorthCentralUs":
+				return { messageValue: ["frankfurt"] };
+				
+			case "NorthCentralUs":
             case "CentralUs":
             case "SouthCentralUs":
 				return { messageValue: ["dallas"] };
+				
+			case "EastUs":
+            case "EastUs2":
+				return { messageValue: ["washington_dc"] };
+				
+			case "WestUs":
+				return { messageValue: ["san_jose"] };
+
+			case "BrazilSouth":
+				return { messageValue: ["washington_dc"] };
+				
+            case "AustraliaEast":
+            case "AustraliaSoutheast":
+				return { messageValue: ["san_jose"] };
+			
+            case "SoutheastAsia":
+				return { messageValue: ["frankfurt"] };
 
             case "ChinaEast2":
             case "ChinaNorth2":
-            case "EastAsia":
+			case "EastAsia":
+				return { messageValue: ["frankfurt"] };
+            
             case "JapanEast":
             case "JapanWest":
-				return { messageValue: ["hong_kong"] };
-
-            case "EastUs":
-            case "EastUs2":
-				return { messageValue: ["washington_dc"] };
+				return { messageValue: ["san_jose"] };
 
             case "SouthAfricaNorth":
-				return { messageValue: ["madrid"] };
-
-            case "WestUs":
-				return { messageValue: ["frankfurt", "amsterdam", "london"] };
 				return { messageValue: ["san_jose"] };
         }
     }
 
-    return { messageValue: ["whocares"] };
+    return { messageValue: ["frankfurt"] };
 
 };
 
